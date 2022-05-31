786dd402543feb724201b118b09286dcb4b71065
Douglas Stewart <dstewart@Douglass-MacBook-Air.local>
2022-05-31 14:18:58 -0400

    Fixed bug causing ModifiedBG.target() to be incorrect

--- inverse_problems/ModifiedBG.py
+++ inverse_problems/ModifiedBG.py
@@ -3,26 +3,23 @@ Hansen, Lupo, and Tantalo claim a modification of the Backus-Gilbert method
     used for determining the inverse of the Laplace transform (Hansen, et al.;
     arxiv:1903.06476) that is able to more reliably extract statistics from 
     input correlator data by using a target smering function as the input.
-
-    This will probably be replaced by a more-faithful-to-the-paper version to ensure
-        everything is working properly
 """
 class ModifiedBG(object):
     
     def __init__(self, data, t, E_0, sigma=0.0, lam=0.0, sym=False):
-        if 0 in t:
-            raise ValueError("Undefined when t=0.")
-
         self.t = mpm.matrix(t)
         self.data = mpm.matrix(gv.mean(data))
-        self.lam = lam
-        self.sigma = sigma
-        self.E_0 = E_0
+        self.lam = mpm.mpf(lam)
+        self.sigma = mpm.mpf(sigma)
+        self.E_0 = mpm.mpf(E_0)
+        self.T = self.t.rows
+        self.sym = sym
         try:
             cov = gv.evalcov(data)
             self.cov = mpm.matrix(cov)
         except AttributeError:
             self.cov = None
+        
             
     """
     Getters
@@ -31,6 +28,9 @@ class ModifiedBG(object):
     """
     def get_t(self):
         return self.t.copy()
+    
+    def get_T(self):
+        return self.T
     
     def get_data(self):
         return self.data.copy()
@@ -46,6 +46,9 @@ class ModifiedBG(object):
     
     def get_E_0(self):
         return self.E_0
+    
+    def get_sym(self):
+        return self.sym
     """
     *
     *
@@ -59,6 +62,7 @@ class ModifiedBG(object):
     """
     def set_t(self, t):
         self.t = mpm.matrix(t)
+        self.T = self.t.rows
     
     def set_data(self, data):
         self.data = mpm.matrix(gv.mean(data))
@@ -79,16 +83,16 @@ class ModifiedBG(object):
     """
     def set_lambda(self, lam):
         if (lam >= 0) and (lam <= 1):
-            self.lam = lam
+            self.lam = mpm.mpf(lam)
         else:
             msg = "Error: lambda must be in the interval [0,1]."
             raise ValueError(msg)
             
     def set_sigma(self, sigma):
-        self.sigma = sigma
+        self.sigma = mpm.mpf(sigma)
     
     def set_E_0(self, E_0):
-        self.E_0 = E_0
+        self.E_0 = mpm.mpf(E_0)
     """
     *
     *
@@ -103,10 +107,11 @@ class ModifiedBG(object):
     def target(self, E, E_star):
         sigma = self.get_sigma()
         Z = self.Z(E_star)
-        const = 1 / (mpm.sqrt(2) * sigma * Z)
-        arg = (-(E - E_star)**2) / (2 * sigma**2)
+        const = mpm.fdiv(1, mpm.fmul(mpm.sqrt(mpm.fmul(2,mpm.pi)), mpm.fmul(sigma, Z)))
+        arg = (-1*mpm.fmul(mpm.fsub(E, E_star),mpm.fsub(E, E_star)))
+        arg = mpm.fdiv(arg, mpm.fmul(2, mpm.fmul(sigma, sigma)))
         
-        return const * mpm.exp(arg)
+        return mpm.fmul(const, mpm.exp(arg))
     
     def delta_bar(self, E, E_star):
         g = self.g(E_star)
@@ -119,22 +124,27 @@ class ModifiedBG(object):
     Auxillary functions
     *
     *
-    """
+    """         
     def Z(self, E_star):
-        sigma = self.sigma
-        arg = E_star / (sigma * mpm.sqrt(2))
-        return 0.5 * (1 + mpm.erf(arg))
+        sigma = self.get_sigma()
+        arg = mpm.fdiv(E_star, mpm.fmul(sigma, mpm.sqrt(2)))
+        return mpm.fdiv(mpm.fadd(1, mpm.erf(arg)), 2)
     
     def N(self, E_star):
         t = self.get_t()
         lam = self.get_lam()
         sigma = self.get_sigma()
         out = self.get_t()
+        Z = self.Z(E_star)
         
-        coef = (1 - lam) / (2 * self.Z(E_star))
+        coef = mpm.fdiv(mpm.fsub(1, lam), mpm.fdiv(2, Z))
         for i in range(out.rows):
-            exponent = -t[i] * (2 * E_star - t[i] * sigma**2) / 2
-            out[i] = coef * mpm.exp(exponent)
+            arg = mpm.fmul(2, E_star)
+            arg = mpm.fsub(arg, mpm.fmul(t[i], mpm.fmul(sigma, sigma)))
+            arg = mpm.fdiv(arg, 2)
+            arg = mpm.fmul(arg, -t[i])
+            #arg = -t[i] * (2 * E_star - t[i] * sigma**2) / 2
+            out[i] = coef * mpm.exp(arg)
         return out
     
     def F(self, E_star):
@@ -144,16 +154,27 @@ class ModifiedBG(object):
         out = self.get_t()
         
         for i in range(out.rows):
-            arg = (-t[i] * sigma ** 2 + E_star - E_0) / (mpm.sqrt(2) * sigma)
-            out[i] = 1 + mpm.erf(arg)
+            arg = mpm.fmul(-t[i], mpm.fmul(sigma, sigma))
+            arg = mpm.fadd(arg, E_star)
+            arg = mpm.fsub(arg, E_0)
+            arg = mpm.fdiv(arg, mpm.fmul(mpm.sqrt(2),sigma))
+            #arg = (-t[i] * sigma ** 2 + E_star - E_0) / (mpm.sqrt(2) * sigma)
+            out[i] = mpm.fadd(1, mpm.erf(arg))
         return out
     
     def f(self, E_star):
+        t_max = self.t_max()
+        T = self.get_T()
+        sym = self.get_sym()
+        
         N = self.N(E_star)
         F = self.F(E_star)
-        out = self.get_t()
-        for i in range(out.rows):
-            out[i] = N[i] * F[i]
+        out = self.get_t()[:t_max]
+        for i in range(t_max):
+            out[i] = mpm.fmul(N[i + 1], F[i + 1])
+            if sym:
+                out[i] = mpm.fadd(out[i], mpm.fmul(N[T - i - 1], F[T - i - 1]))
+                
         return out
     
     """
@@ -163,14 +184,31 @@ class ModifiedBG(object):
     """
     
     """
+    Used for determining t_max as defined in HLT
+    """
+    def t_max(self):
+        T = self.get_T()
+        sym = self.get_sym()
+        if sym:
+            return int(T / 2)
+        return T - 1
+    
+    """
     Response kernel, corresponds to "basis" function or Will's "r" method
     E: float - energy at which the kernel is evaluated
     """
     def r(self, E):
         t = self.get_t()
-        out = self.get_t()
-        for i in range(out.rows):
-            out[i] = mpm.exp(-E*out[i])
+        T = self.get_T()
+        sym = self.get_sym()
+        t_max = self.t_max()
+        
+        out = self.get_t()[:t_max]
+        
+        for i in range(t_max):
+            out[i] = mpm.exp(mpm.fmul(-E,t[i + 1]))
+            if sym:
+                out[i] += mpm.exp(mpm.fmul(-E,mpm.fsub(T, t[i + 1])))
         return out
     
     """
@@ -178,9 +216,16 @@ class ModifiedBG(object):
     """
     def R(self):
         t = self.get_t()
-        out = self.get_t()
-        for i in range(out.rows):
-            out[i] = mpm.fdiv(1, t[i])
+        T = self.get_T()
+        sym = self.get_sym()
+        t_max = self.t_max()
+        
+        out = self.get_t()[:t_max]
+        for i in range(t_max):
+            out[i] = mpm.fdiv(1, (t[i + 1]))
+            if sym:
+                
+                out[i] += mpm.fdiv(1, mpm.fsub(T, t[i + 1]))
         return out
     
     """
@@ -189,16 +234,24 @@ class ModifiedBG(object):
     """
     def A(self):
         t = self.get_t()
+        T = self.get_T()
         E_0 = self.get_E_0()
-        size = t.rows
-        A = mpm.matrix(size, size)
+        t_max = self.t_max()
+        sym = self.get_sym()
+        
+        A = mpm.matrix(t_max, t_max)
 
-        for i in range(size):
-            for j in range(size):
-                t_sum = t[i] + t[j]
+        for i in range(t_max):
+            for j in range(t_max):
+                t_sum = mpm.fadd(t[i + 1], t[j + 1])
+                #t_dif = mpm.fsub(t[i + 1], t[j + 1])
                 try:
                     # Expressions for the Laplace kernel only
-                    A[i, j] = mpm.fdiv(mpm.exp(-E_0*t_sum), t_sum)
+                    A[i, j] = mpm.fdiv(mpm.exp(mpm.fmul(-E_0,t_sum)), t_sum)
+                    if sym:
+                        A[i, j] += mpm.fdiv(mpm.exp(-E_0*(T - t[j + 1] + t[i + 1])), T - t[j + 1] + t[i + 1]) + \
+                            mpm.fdiv(mpm.exp(-E_0*(T - t[i + 1] + t[j + 1])), T - t[i + 1] + t[j + 1]) + \
+                            mpm.fdiv(mpm.exp(-E_0*(2 * T - t[j + 1] - t[i + 1])), 2 * T - t[j + 1] - t[i + 1])
                 except ZeroDivisionError:
                     raise ValueError("Error: spreak matrix requires positive t.")
         return A
@@ -209,15 +262,21 @@ class ModifiedBG(object):
     E: float - energy at which kernel is evaluated
     """
     def W(self, E):
+        data = self.get_data()
+        t_max = self.t_max()
         cov = self.get_cov()
         E_0 = self.get_E_0()
         A = self.A()
         lam = self.get_lam()
         if (self.get_lam() > 0.0 and cov is not None):
-            W = (1 - lam) * A + lam * cov
+            #print(A.rows, A.cols)
+            #print(cov.rows, cov.cols)
+            A_part = mpm.fmul(mpm.fsub(1, lam), A[:t_max,:t_max])
+            B_part = mpm.div(mpm.fmul(lam, cov[:t_max,:t_max]), mpm.fmul(data[0], data[0]))
+            W = mpm.fadd(A_part, B_part)
             #W = A + lam * cov
         else:
-            W = A
+            W = A[:t_max,:t_max]
         return W
     
     """
@@ -225,13 +284,16 @@ class ModifiedBG(object):
     E: float - energy at which the kernel is evaluated
     """
     def g(self, E):
-        W = self.W(E)
-        f = self.f(E)
-        R = self.R()
+        t_max = self.t_max()
+        W = self.W(E)#[:t_max,:t_max]
+        f = self.f(E)#[:t_max]
+        R = self.R()#[:t_max]
+        
+        #print(W.rows, f.rows, R.rows)
         
         Winv = W**-1
         denom = (R.T*Winv*R)[0]
-        numer = 1 - (R.T * Winv * f)
+        numer = mpm.fsub(1, (R.T * Winv * f)[0])
         return Winv * f + Winv * R * (numer / denom)
     
     """
@@ -239,7 +301,8 @@ class ModifiedBG(object):
     g: mpmath matrix object - output coefficients of the method
     """
     def B_functional(g):
-        cov = self.get_cov()
+        t_max = self.t_max()
+        cov = self.get_cov()[:t_max][:t_max]
         return (g.T * cov * g)[0]
     
     """
@@ -248,8 +311,9 @@ class ModifiedBG(object):
     E: float - energy at which the kernel is evaluated
     """
     def reconstruct(self, E):
-        data = self.get_data()
-        g = self.g(E)
+        t_max = self.t_max()
+        data = self.get_data()[1:t_max+1]
+        g = self.g(E)[:t_max]
         
         return (data.T * g)[0]
     
@@ -273,12 +337,6 @@ class ModifiedBG(object):
         reconstruct = self.reconstruct(E)
         return delta * reconstruct
     
-    """
-    IN PROGRESS!
-    HLT suggest a method for determining statistical errors
-    TODO: Actually get this working, this isn't playing well unless the errors
-        are just astronomical
-    """
     def uncertainty(self, E):
         """
         cov = self.get_cov()
@@ -297,11 +355,6 @@ class ModifiedBG(object):
         tot_uncertainty = 0.68 * self.syst_uncertainty(E)
         return tot_uncertainty
     
-
-    """
-    IN PROGRESS!
-    TODO: Fix the uncertainty() method
-    """
     def error(self, E):
         uncertainty = self.uncertainty(E)
         return mpm.sqrt(abs(uncertainty))
@@ -313,6 +366,6 @@ class ModifiedBG(object):
     def solve(self, E):
         avgs = [self.reconstruct(E_star) for E_star in E]
         errs = [self.error(E_star) for E_star in E]
-        #print(avgs)
-        #print(errs)
+        ##print(avgs)
+        ##print(errs)
         return gv.gvar(avgs, errs)
